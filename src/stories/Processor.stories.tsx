984fcac1ea5afa4151caaea23440290d7fc3bc2d
Justin Mahar <contact@justinmahar.com>
2024-02-08 15:33:50 -0500

    Don't show addons panel

--- src/stories/Processor.stories.tsx
+++ src/stories/Processor.stories.tsx
@@ -6,6 +6,9 @@ const StoryComponent = ProcessorComponent; // <-- Set to your component
 const meta: Meta<typeof StoryComponent> = {
   title: 'Utilities/AI Text Processor', // <-- Set to your story title
   component: StoryComponent,
+  parameters: {
+    options: { showPanel: false }, // Don't show addons panel
+  },
 };
 export default meta;
 type Story = StoryObj<typeof meta>;
--- src/stories/Settings.stories.tsx
+++ src/stories/Settings.stories.tsx
@@ -6,6 +6,9 @@ const StoryComponenet = SettingsComponent; // <-- Set to your component
 const meta: Meta<typeof StoryComponenet> = {
   title: 'Utilities/AI Text Processor', // <-- Set to your story title
   component: StoryComponenet,
+  parameters: {
+    options: { showPanel: false }, // Don't show addons panel
+  },
 };
 export default meta;
 type Story = StoryObj<typeof meta>;
