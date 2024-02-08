dbb21f07aba92f205ace13055da40ec8e7aa48e3
Justin Mahar <contact@justinmahar.com>
2024-02-08 14:48:04 -0500

    Clean up

--- src/stories/Processor.stories.tsx
+++ src/stories/Processor.stories.tsx
@@ -2,16 +2,10 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { AITextProcessor as ProcessorComponent } from '../components/AITextProcessor';
 
 // === Setup ===
-const StoryComponenet = ProcessorComponent; // <-- Set to your component
-const meta: Meta<typeof StoryComponenet> = {
+const StoryComponent = ProcessorComponent; // <-- Set to your component
+const meta: Meta<typeof StoryComponent> = {
   title: 'Utilities/AI Text Processor', // <-- Set to your story title
-  component: StoryComponenet,
-  parameters: {
-    controls: {
-      disabled: true,
-    },
-    options: { showPanel: false },
-  },
+  component: StoryComponent,
 };
 export default meta;
 type Story = StoryObj<typeof meta>;
